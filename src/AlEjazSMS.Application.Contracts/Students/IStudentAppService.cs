67714c3e2502dad76dafeb3ee5824cd802a44547
mannan-tkxel <abdul.mannan@tkxel.com>
2022-11-01 23:33:28 +0500

    Test student service for proxy generation setup

---
+++ src/AlEjazSMS.Application.Contracts/Students/IStudentAppService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Services;
+
+namespace AlEjazSMS.Students
+{
+    public interface IStudentAppService: IApplicationService
+    {
+        string GetStudent();
+    }
+}
---
+++ src/AlEjazSMS.Application/Students/StudentAppService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace AlEjazSMS.Students
+{
+    public class StudentAppService: ApplicationService, IStudentAppService
+    {
+        public StudentAppService()
+        {
+        }
+
+        public string GetStudent()
+        {
+            return "Owais Saleem";
+        }
+    }
+}
